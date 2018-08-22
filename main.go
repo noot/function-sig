96c1c7fd34500ca81d05596933afa49f489c47ee
ebinks <elizabethjbinks@gmail.com>
2018-08-22 14:08:59 -0400

    added --topic command line flag to generate topics from event signature

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"flag"
 
 	"github.com/ethereum/go-ethereum/crypto"
 )
@@ -14,10 +15,29 @@ func GenerateSignature(sig string) (string) {
 	return hex[0:8] // first 4 bytes only
 }
 
+func GenerateTopic(sig string) (string) {
+	bytes := []byte(sig)
+	hash := crypto.Keccak256(bytes)
+	hex := hex.EncodeToString(hash)
+	return hex // first 4 bytes only
+}
+
 func main() {
-	var input string
-	fmt.Println("enter function: ")
-	fmt.Scanln(&input)
-	sig := GenerateSignature(input)
-	fmt.Println(sig)
+	sigPtr := flag.Bool("sig", false, "a bool representing whether to prompt to generate a function signature")
+	topicPtr := flag.Bool("topic", false, "a bool representing whether to prompt for a topic")
+
+	flag.Parse()
+	if *sigPtr || !*topicPtr {
+		var input string
+		fmt.Println("enter function: ")
+		fmt.Scanln(&input)
+		sig := GenerateSignature(input)
+		fmt.Println(sig)
+	} else if *topicPtr {
+		var input string
+		fmt.Println("enter event: ")
+		fmt.Scanln(&input)
+		topic := GenerateTopic(input)
+		fmt.Println(topic)
+	}
 }
